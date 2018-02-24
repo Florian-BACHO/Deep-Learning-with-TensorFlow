d9ba37de4db4a180deb3ac75fbd31bc7e0319325
florian bacho <florian.bacho@epitech.eu>
2018-02-24 15:25:21 +0100

    Added adam optimizer

--- DeepLearning/xorLearn.py
+++ DeepLearning/xorLearn.py
@@ -3,16 +3,16 @@
 import tensorflow as tf
 import numpy as np
 
-learningRate = 0.2
+learningRate = 0.5
 nb_entry = 2
 nb_hidden1 = 2
-nb_out = 1
+nb_out = 2
 nb_exemple = 4
 threshold = 0.01
 
 def get_dataset():
     X = [[0, 0], [1, 0], [0, 1], [1, 1]]
-    y = [[0], [1], [1], [0]] # Reshape to have column values
+    y = [0, 1, 1, 0]
     return X, y
 
 
@@ -21,12 +21,14 @@ def main():
 
     entry = tf.placeholder(tf.float32, shape=[None, nb_entry], name="entry")
     layer1 = tf.layers.dense(entry, nb_hidden1, name="hidden", activation=tf.nn.selu)
-    out = tf.layers.dense(layer1, nb_out, name="output", activation=tf.nn.sigmoid)
+    layer2 = tf.layers.dense(layer1, nb_out, name="hidden2")
+    out = tf.nn.softmax(layer2)
 
-    expectedOuts = tf.placeholder(tf.float32, shape=[None, nb_out], name="expectedOuts")
-    loss = -tf.reduce_mean((expectedOuts * tf.log(out) + (1 - expectedOuts) * tf.log(1 - out)))
+    expectedOuts = tf.placeholder(tf.int32, shape=[None], name="expectedOuts")
+    xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=layer2, labels=expectedOuts)
+    loss = tf.reduce_mean(xentropy)
 
-    optimizer = tf.train.GradientDescentOptimizer(learningRate)
+    optimizer = tf.train.AdamOptimizer(learning_rate=learningRate)
     gradientDescentOp = optimizer.minimize(loss)
 
     init = tf.global_variables_initializer() # Variable initilizer
@@ -36,10 +38,10 @@ def main():
         sess.run(init) # Initialize variables
 
         loss_value = loss.eval(feed_dict={entry: X, expectedOuts: y})
-        print("Current loss:",(loss).eval(feed_dict={entry: X, expectedOuts: y}))
+        print("Current loss:", loss_value)
         i = 0
         while loss_value > threshold:
-            if i % 100 == 0:
+            if i % 1 == 0:
                 print("Iteration", i)
                 loss_value = loss.eval(feed_dict={entry: X, expectedOuts: y})
                 print("Current loss:", loss_value)
