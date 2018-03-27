41872fc7dbc673ad02fa91e7d3b9bf7786db9627
florian bacho <florian.bacho@epitech.eu>
2018-03-27 18:00:52 +0200

    Changed test function in RNN

--- RecurrentNeuralNetwork/BasicRNN.py
+++ RecurrentNeuralNetwork/BasicRNN.py
@@ -1,34 +1,47 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+from math import sin
 
 n_step = 20
 n_input = 1
 n_neurons = 100
 n_outputs = 1
-n_iteration = 1000
-learningRate = 0.01
+n_iteration = 2000
+learningRate = 0.001
 
 n_train_ex = 100
 n_test_ex = 10
 
 def testFunction(x):
-    return x * x * x
+    return sin(x)
 
 def getDataSet():
     x_train = []
     y_train = []
+    x_test = []
+    y_test = []
     x = -5
 
     for i in range(n_train_ex): # Create an exemple
         tmp = []
-        for j in range(n_step): # Append each entry
+        for j in range(n_step): # Append each time entry
             tmp.append([testFunction(x - n_step + j)])
         y_train.append([testFunction(x)])
         x_train.append(tmp)
         x += 0.1
 
-    return x_train, y_train
+    for i in range(n_test_ex):
+        tmp = []
+        for j in range(n_step):
+            tmp.append([testFunction(x - n_step + j)])
+        y_test.append([testFunction(x)])
+        x_test.append(tmp)
+        x += 0.1
 
-x_train, y_train = getDataSet()
+    return x_train, y_train, x_test, y_test
+
+x_train, y_train, x_test, y_test = getDataSet()
 x = tf.placeholder(tf.float32, [None, n_step, n_input])
 y = tf.placeholder(tf.float32, [None, n_outputs])
 
@@ -48,5 +61,11 @@ with tf.Session() as sess:
     for iteration in range(n_iteration):
         sess.run(training_op, feed_dict={x: x_train, y: y_train})
         if iteration % 100 == 0:
-            mse = loss.eval(feed_dict={x: x_train, y: y_train})
+            mse = loss.eval(feed_dict={x: x_test, y: y_test})
             print(iteration, "\tMSE:", mse)
+
+    plt.plot(y_train, "b.")
+    plt.plot(logits.eval(feed_dict={x: x_train}), "r.")
+    plt.plot(y_test, "b.")
+    plt.plot(logits.eval(feed_dict={x: x_test}), "r.")
+    plt.show()
