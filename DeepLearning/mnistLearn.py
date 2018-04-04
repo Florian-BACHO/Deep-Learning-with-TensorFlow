b507e20e3c198d95c16f03335f25c77bf23e6536
florian bacho <florian.bacho@epitech.eu>
2018-04-04 14:27:15 +0200

    Added dropout regularization in mnistLearn

--- DeepLearning/mnistLearn.py
+++ DeepLearning/mnistLearn.py
@@ -5,6 +5,7 @@ from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 
 learningRate = 0.01
+dropout_rate = 0.25
 nb_entry = 28 * 28 # 28 pixels per 28 pixels
 nb_hidden1 = 300
 nb_hidden2 = 100
@@ -12,14 +13,17 @@ nb_out = 10 # 0, 1, 2, ..., 9
 batch_size = 1000
 n_epoch = 500
 
-
 def main():
     mnist = input_data.read_data_sets("/tmp/data/")
 
+    training = tf.placeholder_with_default(False, shape=())
     entry = tf.placeholder(tf.float32, shape=[None, nb_entry])
-    layer1 = tf.layers.dense(entry, nb_hidden1, activation=tf.nn.relu)
-    layer2 = tf.layers.dense(layer1, nb_hidden2, activation=tf.nn.relu)
-    out = tf.layers.dense(layer2, nb_out)
+    entry_drop = tf.layers.dropout(entry, dropout_rate, training=training)
+    layer1 = tf.layers.dense(entry_drop, nb_hidden1, activation=tf.nn.relu)
+    layer1_drop = tf.layers.dropout(layer1, dropout_rate, training=training)
+    layer2 = tf.layers.dense(layer1_drop, nb_hidden2, activation=tf.nn.relu)
+    layer2_drop = tf.layers.dropout(layer2, dropout_rate, training=training)
+    out = tf.layers.dense(layer2_drop, nb_out)
 
     expectedOuts = tf.placeholder(tf.int32, shape=[None])
     xentropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=out, labels=expectedOuts)
@@ -43,7 +47,8 @@ def main():
                 print("Iteration", i, "\tTest Accuracy:", test_accuracy, "%")
             X_batch, y_batch = mnist.train.next_batch(batch_size)
             sess.run(gradientDescentOp, feed_dict={entry: X_batch, \
-                                                   expectedOuts: y_batch})
+                                                   expectedOuts: y_batch,\
+                                                   training: True})
             i += 1
         print("FINISHED", "%\tTest Accuracy:", test_accuracy, "%")
 
